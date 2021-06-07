f043f3d2ccfacc5d3deebce9bd99ce8c65b718f4
sushree007 <69665499+sushree007@users.noreply.github.com>
2021-06-07 13:52:44 +0530

    added initial files

---
+++ index.js
@@ -0,0 +1,72 @@
+// Detecting Button Press
+
+var n=document.querySelectorAll(".drum").length;
+for(var i=0;i<n;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function (){
+        var k=this.innerHTML;
+        makeSound(k);
+        buttonAnimation(k);
+});
+}
+
+
+
+
+// Detecting Keyboard Press
+
+document.addEventListener("keydown",function (event){
+     makeSound(event.key);
+     buttonAnimation(event.key);
+});
+
+
+
+
+
+function makeSound(k){
+    switch (k) {
+        case "w":
+            var tom1=new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2=new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3=new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare=new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash=new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick=new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+    
+        default:
+            break;
+    }
+   
+
+}
+
+
+function buttonAnimation(k){
+    var p=document.querySelector("."+k);
+    p.classList.add("pressed");
+
+    setTimeout(function (){
+        p.classList.remove("pressed");
+    },100);
+}
